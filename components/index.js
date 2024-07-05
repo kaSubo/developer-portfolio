fd3739ae9b34ba54e564508e7541f7dc885860e9
Anton Nikityuk <aivancenko840@gmail.com>
2024-07-05 22:48:26 +0500

    initial commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+export { default as Approach } from './Approach';
+export { default as Clients } from './Clients';
+export { default as Experience } from './Experience';
+export { default as Footer } from './Footer';
+export { default as Grid } from './Grid';
+export { default as Hero } from './Hero';
+export { default as RecentProjects } from './RecentProjects';
+
